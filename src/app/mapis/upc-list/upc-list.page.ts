49d4e67fcc8bec9d5b35b00f09f8aae9f1b900b2
sedric oña <facrson@gmail.com>
2025-05-20 16:38:59 -0500

    nuevos cambios en el archivo principal

--- src/app/mapis/upc-list/upc-list.page.ts
+++ src/app/mapis/upc-list/upc-list.page.ts
@@ -10,39 +10,40 @@ import { Platform } from '@ionic/angular';
 })
 export class UpcListPage implements OnInit {
   upcs: any[] = [];
-  searchTerm: string = '';
+  searchTerm = '';
 
-  constructor(private upcService: UpcService, private navCtrl: NavController, private platform: Platform) {}
+  constructor(
+    private upcService: UpcService,
+    private navCtrl: NavController,
+    private platform: Platform
+  ) {}
 
   ngOnInit() {
-    this.upcService.getUpcs().subscribe(data => {
+    this.upcService.getUpcs().subscribe((data) => {
       this.upcs = data;
     });
   }
-  
 
   get filteredUpcs() {
     if (!this.searchTerm) return this.upcs;
-    return this.upcs.filter(upc =>
-      upc['NOMBRE DEL ESTABLECIMIENTO POLICIAL']?.toLowerCase().includes(this.searchTerm.toLowerCase())
+    return this.upcs.filter((upc) =>
+      upc['NOMBRE DEL ESTABLECIMIENTO POLICIAL']
+        ?.toLowerCase()
+        .includes(this.searchTerm.toLowerCase())
     );
   }
 
- 
   verEnMaps(upc: any) {
     const lat = parseFloat(upc['COORDENADA Y (NORTE)']);
     const lng = parseFloat(upc['COORDENADA X (ESTE)']);
-  
     if (isNaN(lat) || isNaN(lng)) {
       console.warn('❌ Coordenadas inválidas:', lat, lng);
       return;
     }
-  
-    const label = encodeURIComponent(upc['NOMBRE DEL ESTABLECIMIENTO POLICIAL'] || 'Ubicación policial');
-  
+    const label = encodeURIComponent(
+      upc['NOMBRE DEL ESTABLECIMIENTO POLICIAL'] || 'Ubicación policial'
+    );
     const url = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
-  
     window.open(url, '_blank'); // Abre en nueva pestaña o app de Google Maps
   }
-  
 }
--- src/app/my-search/my-search.page.ts
+++ src/app/my-search/my-search.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-search',
@@ -10,31 +10,23 @@ import {map} from 'rxjs/operators';
 export class MySearchPage implements OnInit {
   data: any;
   junk: any;
-  pages :any = [];
+  pages: any = [];
   filterTerm: string;
 
-  constructor(private http: HttpClient) { }
-  
-
+  constructor(private http: HttpClient) {}
   ngOnInit() {
-    this.getUrls().subscribe(res => 
-      {console.log(res);
+    this.getUrls().subscribe((res) => {
+      console.log(res);
       this.pages = res;
-            }      );
+    });
   }
-  dismiss(){
+  dismiss() {
     this.dismiss();
   }
 
-  getUrls(){
+  getUrls() {
     return this.http
-    .get('assets/my-search.json')
-    .pipe(
-      map((res:any)=>{
-        return res.data;
-
-      })
-    )
+      .get('assets/my-search.json')
+      .pipe(map((res: any) => res.data));
   }
-
 }
